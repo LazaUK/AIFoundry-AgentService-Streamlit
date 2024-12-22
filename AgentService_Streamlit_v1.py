79c51581cc5f53231cdba48a1973eb8e07eddb60
Laziz Turakulov <4857092+LazaUK@users.noreply.github.com>
2024-12-22 22:12:31 +0000

    Add files via upload

--- AgentService_Streamlit_v1.py
+++ AgentService_Streamlit_v1.py
@@ -1,6 +1,6 @@
 import os
 from azure.ai.projects import AIProjectClient
-from azure.ai.projects.models import CodeInterpreterTool, ToolSet
+from azure.ai.projects.models import ToolSet, CodeInterpreterTool, BingGroundingTool
 from azure.identity import DefaultAzureCredential
 import streamlit as st
 from PIL import Image
@@ -11,39 +11,47 @@ from PIL import Image
 
 # Set page config
 st.set_page_config(
-    page_title = "AgentService-DemoKit",
+    page_title = "Agent Service - Demo Kit",
     page_icon = ":ninja:"
 )
 
 # Streamlit state to store session state variables
-if "conn_str" not in st.session_state:
-    st.session_state["conn_str"] = os.getenv("AZURE_FOUNDRY_PROJECT_CONNSTRING")
 if "interpreter_image" not in st.session_state:
     st.session_state["interpreter_image"] = ""
 if "interpreter_code" not in st.session_state:
     st.session_state["interpreter_code"] = ""
 if "progress" not in st.session_state:
     st.session_state["progress"] = 0
+env_vars = ["project_connstring", "gpt_model", "bing_search"]
+for var_name in env_vars:
+    if var_name not in st.session_state:
+        st.session_state[var_name] = os.getenv(("AZURE_FOUNDRY_" + var_name).upper())
+missing_vars = [var for var in env_vars if not st.session_state.get(var)]
+if missing_vars:
+    missing_vars_str = ", ".join([("AZURE_FOUNDRY_" + var).upper() for var in missing_vars])
+    st.error(f"Environment variable(s) {missing_vars_str} are not set. Please set them and try again.")
+    st.stop()
+
+# Initialise key Azure AI Foundry variables
+project_connstring = st.session_state.get("project_connstring")
+gpt_model = st.session_state.get("gpt_model")
+bing_search = st.session_state.get("bing_search")
 
 # Set sidebar navigation
 st.sidebar.title("Instructions:")
 st.sidebar.markdown(
     """
-    This Streamlit app is a demo kit for Azure AI Foundry's Agent Service.
+    This Streamlit app is a Demo Kit for Azure AI Foundry's Agent Service.
 
-    Please, ensure that you setup the right environment variables. For details, refer to the source [GitHub page](https://github.com/LazaUK/AIFoundry-AgentService-Streamlit).
+    For source code, setup instructions and more details, visit the [GitHub repo](https://github.com/LazaUK/AIFoundry-AgentService-Streamlit).
     """
 )
 menu = st.sidebar.radio("Choose a capability:", ("Code Interpreter", "Bing Search"))
 
 # Helper Function for Code Interpreter capability
-def code_interpreter(prompt):
+def code_interpreter(prompt, conn_str=project_connstring, model=gpt_model):
     st.session_state["interpreter_image"] = ""
     st.session_state["interpreter_code"] = ""
-    conn_str = st.session_state.get("conn_str")
-    if not conn_str:
-        st.error("Environment variable 'AZURE_FOUNDRY_PROJECT_CONNSTRING' is not set. Please set it and try again.")
-        return "Please set the environment variable 'AZURE_FOUNDRY_PROJECT_CONNSTRING'."
 
     try:
         # Initiate AI Project client
@@ -62,7 +70,7 @@ def code_interpreter(prompt):
 
         # Initiate Agent Service
         agent = project_client.agents.create_agent(
-            model = os.getenv("AZURE_FOUNDRY_GPT_MODEL"),
+            model = model,
             name = "demo-agent",
             instructions = "You are a helpful data analyst. You can use Python to perform required calculations.",
             toolset = toolset
@@ -150,17 +158,7 @@ st.title("Azure AI Foundry's Agent Service Demo Kit")
 
 if menu == "Code Interpreter":
     st.header("Solving challenging problems with sandboxed Python code")
-    default_prompt = """
-    Could you please analyse the operating profit of Contoso Inc. 
-    using the following data and producing a bar chart image? 
-    Contoso Inc. Operating Profit: 
-    Quarter 1: $2.2 million, Quarter 2: $2.5 million, 
-    Quarter 3: $2.3 million, Quarter 4: $3.8 million, 
-    Industry Average: $2.5 million. 
-    When quarter values in 2024 fall below the industry average, 
-    please highlight them in red, otherwise they should be green.
-    """
-    default_prompt = "".join([line.lstrip() for line in default_prompt.splitlines()]) 
+    default_prompt = "Could you please analyse the operating profit of Contoso Inc. using the following data and producing a bar chart image. Contoso Inc. Operating Profit: Quarter 1: $2.2 million, Quarter 2: $2.5 million, Quarter 3: $2.3 million, Quarter 4: $3.8 million, Industry Average: $2.5 million. When quarter values in 2024 fall below the industry average, please highlight them in red, otherwise they should be green."
     prompt = st.text_area("Enter your prompt:", value=str(default_prompt), height=150)
     if st.button("Run"):
         st.session_state.progress = 0
@@ -177,7 +175,7 @@ if menu == "Code Interpreter":
         st.session_state['interpreter_code'] = ''
 
 elif menu == "Bing Search":
-    st.header("Grounding with real-time Bing Search results")
+    st.header("Grounding output with real-time Bing Search results")
     prompt = st.text_area("Enter your search query:", value="Can you provide a summary of the 2024 Formula 1 season, including the key highlights?", height=150)
     if st.button("Run"):
         result = bing_search(prompt)
